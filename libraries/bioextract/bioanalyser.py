0259ecb64e1a8bf8bbba96783e6b334ad03d80f7
Pou <ignemmram@gmail.com>
2021-12-02 22:08:50 -0600

    Creacion de la funcion get_taxid

--- libraries/bioextract/bioanalyser.py
+++ libraries/bioextract/bioanalyser.py
@@ -24,8 +24,37 @@ CATEGORY
 
 """
 
+from Bio import Entrez
+
 
 # Crear la excepcion AmbiguousBaseError
 class AmbiguousError(Exception):
     """Clase para el manejo de errores de distintos tipos."""
     pass
+
+
+def get_taxid(email: str, species: list[str]):
+    """Obtener los TaxID"""
+
+    # Definir el correo necesario para la busqueda
+    Entrez.email = email
+
+    # Realizar la busquedas de ids
+    ids = []
+    for specie in species:
+        # Guardar la busqueda
+        handle = Entrez.esearch(term=specie, db="taxonomy",
+                                retmode="xml")
+        # Obtener la estructura del archivo XML de la busqueda
+        record = Entrez.read(handle)
+
+        # Evaluar si la lista esta vacia
+        if len(record['IdList']) == 0:
+            raise SystemExit(
+                f"Organismo '{specie}' no encontrado")
+        else:
+            # Guardar el id
+            ids.append(record['IdList'][0])
+
+    # Devolver los IDs
+    return ids
