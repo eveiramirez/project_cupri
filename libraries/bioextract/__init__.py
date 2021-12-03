6f77a2e8550b042c52624f38680c5dbd2309613b
Pou <ignemmram@gmail.com>
2021-12-02 22:24:32 -0600

    Creacion del archivo __init__.py

---
+++ libraries/bioextract/__init__.py
@@ -0,0 +1,34 @@
+"""
+PACKAGE NAME
+        bioextract
+
+VERSION
+        [0.0.1]
+
+PYTHON VERSION
+        3.9
+
+AUTHORS
+        Ignacio Emmanuel Ramirez Bernabe
+        Diego
+        Melissa
+
+CONTACT
+        iramirez@lcg.unam.mx
+
+DESCRIPTION
+        bioanalyser es una paqueteria que contiene distintas
+        herramientas para el manejo de información de bases de datos
+        de NCBI, enfocadas principalmente hacia bacterias
+
+CATEGORY
+        Bacteria
+        DNA
+        Protein
+        Sequence
+
+"""
+
+from .bioanalyser import (get_taxid, get_tax_data)
+
+__all__ = ['get_taxid', 'get_tax_data', ]
