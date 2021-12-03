120758463111261b8fe2a39f9a8d7c88c42a455e
Pou <ignemmram@gmail.com>
2021-12-02 23:48:54 -0600

    Creacion del modulo de protein

---
+++ libraries/bioextract/protein.py
@@ -0,0 +1,26 @@
+"""
+MODULE NAME
+        protein
+
+VERSION
+        [0.0.1]
+
+PYTHON VERSION
+        3.9
+
+AUTHOR
+        Ignacio Emmanuel Ramirez Bernabe
+        Diego
+        Melissa
+
+CONTACT
+        iramirez@lcg.unam.mx
+
+DESCRIPTION
+        Modulo para la obtencion de informacion de proteinas.
+
+CATEGORY
+        Protein
+"""
+
+from .bioanalyser import (get_taxid)
