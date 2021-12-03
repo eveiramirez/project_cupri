f7af96a38404b2e46ab68b28ca6ab5f299a01111
Pou <ignemmram@gmail.com>
2021-12-02 23:38:13 -0600

    Creacion del modulo dna

---
+++ libraries/bioextract/dna.py
@@ -0,0 +1,26 @@
+"""
+MODULE NAME
+        dna
+
+VERSION
+        [0.0.1]
+
+PYTHON VERSION
+        3.9
+
+AUTHORS
+        Ignacio Emmanuel Ramirez Bernabe
+        Diego
+        Melissa
+
+CONTACT
+        iramirez@lcg.unam.mx
+
+DESCRIPTION
+        Modulo para la obtencion de informacion de genomas.
+
+CATEGORY
+        DNA
+"""
+
+from .bioanalyser import (get_taxid)
