265ec6cf8eb1a79be7e0476702aba52f78c3a043
Pou <ignemmram@gmail.com>
2021-12-02 22:07:00 -0600

    Creación del modulo bioanalyser

---
+++ libraries/bioextract/bioanalyser.py
@@ -0,0 +1,25 @@
+"""
+MODULE NAME
+        bioanalyser
+
+VERSION
+        [0.0.1]
+
+PYTHON VERSION
+        3.9
+
+AUTHORS
+        Ignacio Emmanuel Ramirez Bernabe
+
+CONTACT
+        iramirez@lcg.unam.mx
+
+DESCRIPTION
+        Modulo principal para análisis de archivos
+
+CATEGORY
+        Extractor
+        Analyser
+        Sequence
+
+"""
